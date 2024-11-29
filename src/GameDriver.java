13e4dd368472dd74a49b646b4cf9d0719f88a7c1
Christian J. Dennis <166155393+ChristianDenniss@users.noreply.github.com>
2024-11-29 09:52:26 -0400

    Update GameDriver.java

--- src/GameDriver.java
+++ src/GameDriver.java
@@ -12,6 +12,37 @@ public class GameDriver
 {
    public static void main(String[] args)
    {
+		//variables / declaration
+		//we are going to need user input so make a scanner object, declare intent of a deck object
+		//intialize a Hand object for both the player and dealer, create a int for number of decks parameter input
+		//and boolean isDealer for our hand print method
+		
       Scanner myScanner = new Scanner(System.in);
+		Deck myDeck;
+		Hand dealerHand = new Hand();
+		Hand playerHand = new Hand();
+		int numberOfDecks, playerSum, dealerSum;
+		boolean isDealer;
+		
+		// Create a shuffled deck of cards consisting of 6 standard decks
+		numberOfDecks = 6;
+		myDeck = new Deck(numberOfDecks);
+		
+		//use shuffle method to make sure cards are shuffled, we can do this in the Deck class or manually here
+		myDeck.shuffle();
+		
+		//Deal a card the players hand, a card to the dealers hand, a second card to both
+		//Deal a card using our dealCard method add it to our player hand using 
+		
+		playerHand.addCard(myDeck.dealCard());
+		dealerHand.addCard(myDeck.dealCard());
+		playerHand.addCard(myDeck.dealCard());
+		dealerHand.addCard(myDeck.dealCard());
+		
+		//display to the user the sum of their current hand using our sum hand method
+		playerSum = dealerHand.sumHand();
+		
+		
+		
    }
 }
